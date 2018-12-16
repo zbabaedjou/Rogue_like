2131e3e005d8fb08b6a679c7238e6265981e51e3
nfndame <nfndame@gmail.com>
2018-12-16 17:53:44 +0100

    ClasseUnitaire TestGenerUnivers

---
+++ src/main/java/roguelike/TestGenerUnivers.java
@@ -0,0 +1,23 @@
+//Class essaie
+package roguelike
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGenerUnivers {
+
+	@Test
+	public void test() 
+	{
+		private GenerUnivers gener;
+		
+		generer= new GenerUnivers (4,7);
+		
+	}
+	public void testGenerer()
+	{
+		Assert.assrtNotNull (gener.randomizeElement());
+	
+	}	
+}
